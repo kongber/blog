7c263e5ce7b1973b61d5ae359bf7f6f69553733f
Kongber <kongmongolia@gmail.com>
2025-04-08 18:57:05 +0800

    Auto commit on 2025-04-08 18:57:05

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -6,7 +6,15 @@ export const navbar = defineNavbarConfig([
   { text: '标签', link: '/blog/tags/' },
   { text: '归档', link: '/blog/archives/' },
   {
-    text: '笔记',
+    text: '计算机系统',
+    items: [{ text: '示例', link: '/notes/demo/README.md' }]
+  },
+  {
+    text: '计算机网络',
+    items: [{ text: '示例', link: '/notes/demo/README.md' }]
+  },
+  {
+    text: '计算机安全',
     items: [{ text: '示例', link: '/notes/demo/README.md' }]
   },
 ])
---
+++ docs/notes/01.system/README.md
@@ -0,0 +1,8 @@
+---
+title: Demo
+createTime: 2025/04/08 13:40:52
+permalink: /01.system/
+---
+
+- [bar](./bar.md)
+- [foo](./foo.md)
