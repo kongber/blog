f9770668b390163bd99ab87f9e27eceb01aa186c
Kongber <kongmongolia@gmail.com>
2025-04-09 10:12:18 +0800

    Auto commit on 2025-04-09 10:12:18

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,9 +2,9 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
   { text: '首页', link: '/' },
-  { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  // { text: '博客', link: '/blog/' },
+  // { text: '标签', link: '/blog/tags/' },
+  // { text: '归档', link: '/blog/archives/' },
   {
     text: '计算机基础',
     items: [
